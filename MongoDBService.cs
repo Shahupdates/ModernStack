e09104ef5fc71170c878cfa18a6446d2b0ffde4c
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-01 10:40:58 -0400

    Update MongoDBService.cs
    add methods to perform CRUD operations on mongoDB collections

--- MongoDBService.cs
+++ MongoDBService.cs
@@ -9,4 +9,11 @@ public class MongoDbService
         var client = new MongoClient("mongodb://localhost:27017");
         _database = client.GetDatabase("myDatabase");
     }
+    
+    // metod() to perform crud operations to mongoDB collection: insert a document
+    public void InsertDocument<T>(string collectionName, T document)
+    {
+        var collection = _database.GetCollection<T>(collectionName);
+        collection.InsertOne(document);
+    }
 }
